a375643b1b9b5ce129c4f1c94b3e12db67f05acd
Jevgeni Tsaikin <jevgeni@outlook.com>
2012-11-12 16:17:18 +0100

    simple Timer is added to Common project

---
+++ WPPerfLab/WPPerfLab.Common/Profiling/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPPerfLab.Common.Profiling
+{
+    public static class Timer
+    {
+        private static Stopwatch sw = new Stopwatch();
+
+        public static void Start()
+        {
+            sw.Start();
+        }
+
+        public static void Stop()
+        {
+            sw.Stop();
+#if DEBUG
+            Debug.WriteLine("Duration: {0} ms", sw.ElapsedMilliseconds);
+#endif
+        }
+    }
+}
