ad1dfd47d785f4408f73bb77b4e17dc7daf1b3c0
Jevgeni Tsaikin <jevgeni@outlook.com>
2012-11-14 18:51:24 +0100

    Modified ListSamples.

---
+++ WPPerfLab/WPPerfLab.Samples/ListSamples/NonVirtualizedListSampleView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Phone.Controls;
+using WPPerfLab.Common.Entities;
+
+namespace WPPerfLab.Samples.ListSamples
+{
+    public partial class NonVirtualizedListSampleView : PhoneApplicationPage
+    {
+        public NonVirtualizedListSampleView()
+        {
+            InitializeComponent();
+
+            Loaded += (s, e) =>
+            {
+                ProductRepository.GetProducts(100, 0).ForEach(p => itemList.Items.Add(p));
+            };
+        }
+    }
+}
---
+++ WPPerfLab/WPPerfLab.Samples/ListSamples/NonVirtualizedStackPanel.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+using Microsoft.Phone.Controls;
+
+namespace WPPerfLab.Samples.ListSamples
+{
+    public partial class NonVirtualizedStackPanel : PhoneApplicationPage
+    {
+        public NonVirtualizedStackPanel()
+        {
+            InitializeComponent();
+            Loaded += (s, e) =>
+                {
+                    long height = 25;
+                    for (int i = 0; i < 1600; i++)
+                    {
+                        itemList.Items.Add(height);
+                    }
+                };
+        }
+    }
+}
